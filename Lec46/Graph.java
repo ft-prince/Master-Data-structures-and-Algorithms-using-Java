f7af12c0a5736aadcfb2837af55cd449cd84621f
Prince <128713671+ft-prince@users.noreply.github.com>
2023-05-29 18:54:34 +0530

    Graph.java

--- Lec46/Graph.java
+++ Lec46/Graph.java
@@ -111,5 +111,101 @@ public boolean BFS(int src, int des) {
        q.add(nbrs);
       }
     }
+  }
     return false;
   }
+	
+	
+	public boolean DFS(int src, int des) {
+		Stack<Integer> s = new Stack<>();
+		HashSet<Integer> visited = new HashSet<>();
+		s.push(src);
+		while (!s.isEmpty()) {
+			// 1 remove;
+			int rv = s.pop();
+			// 2 Ignore if Already visited
+			if (visited.contains(rv)) {
+				continue;
+			}
+			// 3 visited marked
+			visited.add(rv);
+			// 4 Self Work
+			if (rv == des) {
+				return true;
+			}
+			// Nbrs Add krna
+			for (int nbrs : map.get(rv).keySet()) {
+				if (!visited.contains(nbrs)) {
+					s.push(nbrs);
+				}
+			}
+
+		}
+		return false;
+
+	}
+public void BFT() {
+		Queue<Integer> q = new LinkedList<>();
+		HashSet<Integer> visited = new HashSet<>();
+		for (int src : map.keySet()) {
+			if (visited.contains(src)) {
+				continue;
+			}
+			q.add(src);
+			while (!q.isEmpty()) {
+				// 1 remove;
+				int rv = q.poll();
+				// 2 Ignore if Already visited
+				if (visited.contains(rv)) {
+					continue;
+				}
+				// 3 visited marked
+				visited.add(rv);
+				// 4 Self Work
+				System.out.print(rv + " ");
+				// Nbrs Add krna
+				for (int nbrs : map.get(rv).keySet()) {
+					if (!visited.contains(nbrs)) {
+						q.add(nbrs);
+					}
+				}
+
+			}
+		}
+		System.out.println();
+	}
+
+	public void DFT() {
+		Stack<Integer> s = new Stack<>();
+		HashSet<Integer> visited = new HashSet<>();
+		for (int src : map.keySet()) {
+			if (visited.contains(src)) {
+				continue;
+			}
+			s.push(src);
+			while (!s.isEmpty()) {
+				// 1 remove;
+				int rv = s.pop();
+				// 2 Ignore if Already visited
+				if (visited.contains(rv)) {
+					continue;
+				}
+				// 3 visited marked
+				visited.add(rv);
+				// 4 Self Work
+				System.out.print(rv + " ");
+				// Nbrs Add krna
+				for (int nbrs : map.get(rv).keySet()) {
+					if (!visited.contains(nbrs)) {
+						s.push(nbrs);
+					}
+				}
+
+			}
+		}
+
+		System.out.println();
+	}
+
+}
+
